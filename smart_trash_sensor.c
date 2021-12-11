4cc1f2043916bba07604aecd044699eb1ac27f49
LEE-YOUNG-JE <76897007+LEE-YOUNG-JE@users.noreply.github.com>
2021-12-11 18:05:26 +0900

    update trash sensor(before wifi)
    쓰레기인식하면 led깜빡 3번까지 구혐
    아직 와이파이 검증안됨

--- smart_trash_sensor.c
+++ smart_trash_sensor.c
@@ -10,7 +10,7 @@
 
 
 #define _D0_BUTTON_GPIO 2          //BUTTON
-#define _D1_LED_GPIO 15            //LED
+#define _D2_LED_GPIO 26            //LED
 #define _A0_LIGHT_ADC_GPIO 36      //조도센서
 #define _A1_R_GPIO 39              //가변저항
 #define _TOUCH_LEFT 27             //왼쪽 터치센서
@@ -19,6 +19,7 @@
 #define can 1
 #define glass 2
 /*-------------------------------------------wifi function---------------------------*/
+int all_state = 0; //0이면 처음 쓰레기 선택모드상태, 1이면 버튼 눌린상태 = 쓰레기 인식대기상태, 2이면 hicloud로 데이터 보내는 상태
 int pa = 0; //amount of plastic amount
 int ga = 0; //amount of glass
 int ca = 0; //amount of can 
@@ -32,8 +33,8 @@ int button_status=0;
 int val = 0;
 int old_val = 0;
 int state = 0;    
-int led_val = 1;
-int led_old_val = 1;
+int shadow_val = 1;
+int shadow_old_val = 1;
 int led_state = 0;            //사용할 변수 선언
 const char* ssid = "Bong";
 const char* password = "";
@@ -124,46 +125,42 @@ static const uint8_t PROGMEM initial[5][9]={
 void setup() {
 /*------------------sensor setup---------------------*/  
   Serial.begin(115200);
-  pinMode(15,OUTPUT);//없어도됨 문자잘오는지 확인용
- digitalWrite(15,LOW);//없어도됨 문자잘오는지 확인용
+  pinMode(15,OUTPUT);//
+ digitalWrite(15,LOW);// 쓰레기통 open이면 HIGH
   pinMode(_D0_BUTTON_GPIO, INPUT);  // BUTTON 입력설정
-  pinMode(_D1_LED_GPIO, OUTPUT);    // LED 출력설정
-  digitalWrite(_D1_LED_GPIO, LOW);
+  pinMode(_D2_LED_GPIO, OUTPUT);    // LED 출력설정
+  digitalWrite(_D2_LED_GPIO, LOW);
   light=analogRead(_A0_LIGHT_ADC_GPIO); // 초기 조도값 설정
   
   matrix.begin();
 /*---------------------------------------------------*/ 
 }
 void loop() {
-  char CO2String[8];
-  char saving_energyString[8];
-  char EPRString[8];
-  /*---------------------터치센서를 이용하여 led 메트릭스 제어-----------------------------------------------------------loop-------------*/
   matrix.setTextSize(1);
   matrix.setTextColor(100);
-  /*--------------------------------------------------------------------------------------------------------------button_status == 0*/
-  if(button_status == 0){
-  if(touchRead(_TOUCH_LEFT)<11 ){
-     sel_trash++;
-     if(sel_trash>2) sel_trash=0;
+    /*---------------------터치센서를 이용하여 led 메트릭스 제어-----------------------------------------------------------loop-------------*/
+  if(all_state == 0){ //쓰레기 선택모드
+      digitalWrite(15, LOW);
+      if(touchRead(_TOUCH_LEFT)<11 ){
+        sel_trash++;
+        if(sel_trash>2) sel_trash=0;
+      }
+      else if(touchRead(_TOUCH_RIGHT)<17 ){
+         sel_trash--;
+        if(sel_trash<0) sel_trash=2;
+      }
+         matrix.clear();
+         matrix.setCursor(1,1);
+         matrix.printf("%c", text[sel_trash]);
   }
-  else if(touchRead(_TOUCH_RIGHT)<17 ){
-     sel_trash--;
-     if(sel_trash<0) sel_trash=2;
-  }
-     matrix.clear();
-     matrix.setCursor(1,1);
-     matrix.printf("%c", text[sel_trash]);
-  }
- /*--------------------------------------------------------------------------------------------------------------button_status == 0 end*/
-delay(500);
+
 
 /*--------------------------------------------------------------------------------------------------------------button_status == 0 && button눌렸을때*/
- if(!digitalRead(_D0_BUTTON_GPIO) && button_status == 0) button_status = 1;  //상태 변화
-/*--------------------------------------------------------------------------------------------------------------button_status == 0 && button눌렸을때 end*/
+ if(!digitalRead(_D0_BUTTON_GPIO) && all_state == 0) all_state = 1;  //상태 변화
+
 /*button status ==1일때 무표정코드 추가해야됨 elseif로 밑에추가*/
 /*---------------------------------------------------111111111111111111111111111111111---------------------------------------button_status == 1*/
- if(button_status == 1) { //조건이 node-red webcam에서 인식한 것과 같을 때로 바꿔야됨, button_status ==1일때는 인식하기 직전이므로 무표정 아래 표정도 웃는 표정으로 바꾸고 무표정은 따로옮기기
+ if(all_state == 1) { //조건이 node-red webcam에서 인식한 것과 같을 때로 바꿔야됨, button_status ==1일때는 인식하기 직전이므로 무표정 아래 표정도 웃는 표정으로 바꾸고 무표정은 따로옮기기
      matrix.clear();
      matrix.setCursor(1,1);
      matrix.drawBitmap(0,0,initial[0], 8,9, 128); //
@@ -172,35 +169,44 @@ delay(500);
     if(temp == '0' || temp == '1' || temp =='P' || temp =='G' || temp =='C') web_trash = temp;
     Serial.println(web_trash);
     if(PGC[sel_trash] == web_trash){ //문법확인 분리수거 일치할때 *********
-       digitalWrite(15, HIGH);
-        if(analogRead(_A0_LIGHT_ADC_GPIO) < 500){ //쓰레기 감지 안되면
-            digitalWrite(_D1_LED_GPIO, LOW);
-            led_val = digitalRead(_D1_LED_GPIO); //LED의 입력값 저장
+        all_state = 2;
+        web_trash = '0';
+    }
+    else if(web_trash != '0' && web_trash != '1' && web_trash != NULL && web_trash != PGC[sel_trash]){ //분리수거 일치하지 않을때 부저울리고 표정 
+      all_state = 0;
+      matrix.clear();
+      matrix.setCursor(1,1);
+      matrix.drawBitmap(0,0,initial[2], 8,9, 128); //화남
+      delay(1500);
+      web_trash = '0';
+    }
+}
+
+
+/*-------------------------------------------------------------------------------------------all_state == 2 */
+if(all_state == 2){
+  digitalWrite(15, HIGH); //쓰레기통 열림
+        if(analogRead(_A0_LIGHT_ADC_GPIO) < 1500){ //쓰레기 감지 안되면
+            
+            shadow_val = LOW; //LED의 입력값 저장
             Serial.println("led val");
-            Serial.println(led_val);
+            Serial.println(shadow_val);
             Serial.println("led old_val");
-            Serial.println(led_old_val);
-            led_old_val = led_val;    //old_val에 val값 저장(버튼 눌림 감지는 매우 빠르게 일어나기 때문에 이 값은 거의 LOW로 항상 저장된다.)
+            Serial.println(shadow_old_val);
+            shadow_old_val = shadow_val;    //old_val에 val값 저장(버튼 눌림 감지는 매우 빠르게 일어나기 때문에 이 값은 거의 LOW로 항상 저장된다.)
         }
-        else if(analogRead(_A0_LIGHT_ADC_GPIO) > 500){   //쓰레기 감지= led 켜짐//수정필요
+        else if(analogRead(_A0_LIGHT_ADC_GPIO) > 1500){   //쓰레기 감지= led 켜짐//수정필요
             R = analogRead(_A1_R_GPIO);  //무게 저장
 
-        /* 수도코드
-         * if(sel_trash == plastic)인데 플라스틱이 드러왔다? -> 행복 표시  
-                               플라스틱이 아닌게 들어왔다?-> 화난표시
-        */    
-            digitalWrite(_D1_LED_GPIO, HIGH);
-            delay(800);
-            digitalWrite(_D1_LED_GPIO, LOW);
           /*--------------------new yj-----count trash-----------------------------*/
-            led_val = digitalRead(_D1_LED_GPIO); //LED의 입력값 저장
+            shadow_val = HIGH; //LED의 입력값 저장
             Serial.println("trash_led val");
-            Serial.println(led_val);
+            Serial.println(shadow_val);
             Serial.println("trash_led old_val");
-            Serial.println(led_old_val);
-            delay(100);       //LED 입력 감지를 위한 시간 여유
+            Serial.println(shadow_old_val);
+           // delay(100);       //LED 입력 감지를 위한 시간 여유
             
-            if((led_val == HIGH) && (led_old_val == LOW)){  //LED켜지면 쓰레기 들어간것임, 이전값이 LOW일 때(즉, 버튼이 눌렸을 때)
+            if((shadow_val == HIGH) && (shadow_old_val == LOW)){  //LED켜지면 쓰레기 들어간것임, 이전값이 LOW일 때(즉, 버튼이 눌렸을 때)
                 Serial.println("pushed_success");
                 
                 delay(100);
@@ -213,22 +219,24 @@ delay(500);
                 Serial.println(pa);
                 Serial.println(ga);
                 Serial.println(ca);
-                button_status = 0; // 상태원상복구 -> p,g,c선택화면
-                
+                all_state = 0; // 상태원상복구 -> p,g,c선택화면
+                digitalWrite(_D2_LED_GPIO, HIGH);
+                delay(100);
+                digitalWrite(_D2_LED_GPIO, LOW);
+                delay(100);
+                digitalWrite(_D2_LED_GPIO, HIGH);
+                delay(100);
+                digitalWrite(_D2_LED_GPIO, LOW);
+                delay(100);
+                digitalWrite(_D2_LED_GPIO, HIGH);
+                delay(100);
+                digitalWrite(_D2_LED_GPIO, LOW);
+                delay(100);
                 //Preprocessing();
             }
-            led_old_val = led_val;    //old_val에 val값 저장(버튼 눌림 감지는 매우 빠르게 일어나기 때문에 이 값은 거의 LOW로 항상 저장된다.)
+            shadow_old_val = shadow_val;    //old_val에 val값 저장(버튼 눌림 감지는 매우 빠르게 일어나기 때문에 이 값은 거의 LOW로 항상 저장된다.)
         }
-        web_trash = '0';
-    }
-    else if(web_trash != '0' && web_trash != '1' && web_trash != NULL && web_trash != PGC[sel_trash]){ //분리수거 일치하지 않을때 부저울리고 표정 
-      matrix.clear();
-      matrix.setCursor(1,1);
-      matrix.drawBitmap(0,0,initial[2], 8,9, 128); //화남
-      delay(1500);
-      web_trash = '0';
-    }
 }
-/*------------------------------------------------------111111111111111111111111111-------------------------------------button_status == 1 end*/
+
 delay(500);
 }
